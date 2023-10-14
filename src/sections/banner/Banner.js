387fbd4ccb0d3309d2f96d0912417f06f5d48119
Isadora Vanderlan <vanderlansantos1991@gmail.com>
2023-10-13 23:49:36 -0300

    incluido banner

--- src/sections/banner/Banner.js
+++ src/sections/banner/Banner.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import styles from './banner.module.css'
 
 const Banner = () => {
   return (
-    <div>
-      
+    <div className={`${styles.Banner} container`}>
+      <span>Decorações</span> 
+      <h1>Brilho da Magia</h1>
+      <p>Sua festa do jeitinho que você sempre imaginou!</p>
     </div>
   );
 };
--- src/sections/header/Header.js
+++ src/sections/header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <div className={`${styles.Header} container`} id='header'>  
       <div>
-        <img className={styles.ImgHeader} src="./img/logo.png" alt="" />
+        <img className={styles.ImgHeader} src="./img/logo.png" alt="logotipo" />
       </div>
 
       <div className={styles.NavHeader}>
