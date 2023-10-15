36d7f855317cf23bb5a47871752d042133001b91
Isadora Vanderlan <vanderlansantos1991@gmail.com>
2023-10-15 01:33:51 -0300

    incluido mapa

--- src/sections/map/Map.js
+++ src/sections/map/Map.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import styles from '@/sections/map/map.module.css'
 
 const Map = () => {
   return (
-    <div>
+    <div className={`${styles.Map} container`}>
+      <div className={styles.MapTexto}>
+        <img src="./img/contato.png" alt="" />
+      </div>
+
+      <div className={styles.MapMap}> 
+        <h2>Observação:</h2>
+        <p>Nosso endereço é somente para retirada, atendimento ao cliente somente via WhatsApp.</p>
+        <img src="./img/map.png" alt="" />
+      </div>
       
     </div>
   )
