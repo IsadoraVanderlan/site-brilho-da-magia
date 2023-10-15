f062cccf4ab2d9419860062d1b3d9848ee0e47db
Isadora Vanderlan <vanderlansantos1991@gmail.com>
2023-10-15 02:02:15 -0300

    incluido parceiros

--- src/sections/parceiros/Parceiros.js
+++ src/sections/parceiros/Parceiros.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import styles from '@/sections/parceiros/parceiros.module.css'
 
 const Parceiros = () => {
   return (
-    <div>
+    <div className={`${styles.Parceiros} container`}>
+      <h2>Nossos Parceiros</h2>
+      <p>Veja abaixos os nossos parceiros de confiança!</p>
       
+      <div className={styles.ParceirosImg}>
+        <div className={styles.ParceirosCaixa}>
+          <img src="./img/ijcode.png" alt="" />
+          <h3>IJCode</h3>
+          <p>A IJCode é uma empresa de desenvolvimento de sites, foi a responsavel pelo desenvolvimento deste site, nós recomendamos</p>
+        </div>
+
+        <div className={styles.ParceirosCaixa}>
+          <img src="./img/ijcode.png" alt="" />
+          <h3>IJCode</h3>
+          <p>A IJCode é uma empresa de desenvolvimento de sites, foi a responsavel pelo desenvolvimento deste site, nós recomendamos</p>
+        </div>
+
+        <div className={styles.ParceirosCaixa}>
+          <img src="./img/ijcode.png" alt="" />
+          <h3>IJCode</h3>
+          <p>A IJCode é uma empresa de desenvolvimento de sites, foi a responsavel pelo desenvolvimento deste site, nós recomendamos</p>
+        </div>
+      </div>
     </div>
   )
 }
